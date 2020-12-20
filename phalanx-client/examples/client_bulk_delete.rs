2eb49ae2fb508fe646fed812d7d92a38a747f7e5
Minoru Osuka <minoru.osuka@gmail.com>
2020-12-20 21:58:51 +0900

    Add bulk_delete function to client library

---
+++ phalanx-client/examples/client_bulk_delete.rs
@@ -0,0 +1,62 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::thread::sleep;
+
+use anyhow::{Context, Result};
+use tokio::time::Duration;
+
+use phalanx_client::client::Client;
+use phalanx_common::log::set_logger;
+use phalanx_discovery::discovery::etcd::{Etcd as EtcdDiscovery, EtcdConfig};
+use phalanx_discovery::discovery::DiscoveryContainer;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    set_logger();
+
+    let discovery_container = DiscoveryContainer {
+        discovery: Box::new(EtcdDiscovery::new(EtcdConfig {
+            endpoints: vec!["http://localhost:2379".to_string()],
+            root: Some("/phalanx".to_string()),
+            auth: None,
+            tls_ca_path: None,
+            tls_cert_path: None,
+            tls_key_path: None,
+        })),
+    };
+
+    let mut client = Client::new(discovery_container).await;
+
+    client
+        .watch("/")
+        .await
+        .with_context(|| "failed to watch key that starts with \"/\"")?;
+
+    sleep(Duration::from_secs(1));
+
+    let mut docs = Vec::new();
+    let file =
+        File::open("./examples/bulk_delete.txt").with_context(|| "failed to read id text")?;
+    for line in BufReader::new(file).lines() {
+        let doc = line.unwrap();
+        docs.push(doc);
+    }
+
+    match client.bulk_delete(docs.iter().map(|id| id.as_str()).collect(), "index0").await {
+        Ok(_) => {
+            println!("ok");
+        }
+        Err(e) => {
+            println!("error: {}", e.to_string());
+        }
+    };
+
+    match client.unwatch().await {
+        Ok(_) => (),
+        Err(e) => println!("error: {}", e.to_string()),
+    };
+
+    sleep(Duration::from_secs(1));
+
+    Ok(())
+}
