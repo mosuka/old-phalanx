356c8b087166001e3d5766bd16d65db6d65bcaba
Minoru OSUKA <minoru.osuka@gmail.com>
2021-01-07 10:10:39 +0900

    Distributed search (#72)
    * Distributed search
    
    * Update tantivy
    
    * Upgrade tantivy

--- phalanx-index/src/index/search_request.rs
+++ phalanx-index/src/index/search_request.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum CollectionType {
     CountAndTopDocs,
     Count,
     TopDocs,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct SearchRequest {
     pub query: String,
     pub from: u64,
